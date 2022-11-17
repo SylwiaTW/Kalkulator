66b4a44503c2d8cab946c0b9eaaf9b0018d8c9dc
SylwiaTW <slvunia@gmail.com>
2022-11-17 11:46:39 +0100

    Interfejs z przyciskami v2

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,47 +1,67 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Kalkulator {
-    JFrame ramka=new JFrame("Kalkulator");
-    JTextField wyswietlacz = new JTextField();
-    Font czcionka = new Font("Arial", Font.BOLD,30);
-    JPanel panel = new JPanel();
+public class Kalkulator implements ActionListener {
 
-    JButton cyfra0 = new JButton("0");
-    JButton cyfra1 = new JButton("1");
-    JButton cyfra2 = new JButton("2");
-    JButton cyfra3 = new JButton("3");
-    JButton cyfra4 = new JButton("4");
-    JButton cyfra5 = new JButton("5");
-    JButton cyfra6 = new JButton("6");
-    JButton cyfra7 = new JButton("7");
-    JButton cyfra8 = new JButton("8");
-    JButton cyfra9 = new JButton("9");
-    JButton dodawanie = new JButton("+");
-    JButton odejmowanie = new JButton("-");
-    JButton mnozenie = new JButton("*");
-    JButton dzielenie = new JButton("/");
-    JButton przecinek = new JButton(",");
-    JButton rownasie = new JButton("=");
-    JButton potega = new JButton ("x\u00B2");
-    JButton pierwiastek = new JButton ("\u221A");
-    JButton del = new JButton("del");
-    JButton clr = new JButton("clr");
-
+    double numer1=0, numer2=0, wynik=0;
+    int obliczenia;
 
     Kalkulator(){       //konstruktor
-        interfejs();
-        dodajPrzyciski();
-    }
 
-    public void interfejs(){
+        JFrame ramka=new JFrame("Kalkulator");
+        JTextField wyswietlacz = new JTextField();
+        JButton[] cyfry = new JButton[10];
+        Font czcionka = new Font("Arial", Font.BOLD,30);
+        JPanel panel = new JPanel();
+
+        //przyciski funkcyjne
+        JButton dodawanie = new JButton("+");
+        JButton odejmowanie = new JButton("-");
+        JButton mnozenie = new JButton("*");
+        JButton dzielenie = new JButton("/");
+        JButton przecinek = new JButton(",");
+        JButton rownasie = new JButton("=");
+        JButton nawias1 = new JButton ("(");
+        JButton nawias2 = new JButton (")");
+        JButton del = new JButton("del");
+        JButton clr = new JButton("clr");
+
+        dodawanie.addActionListener(this);
+        odejmowanie.addActionListener(this);
+        mnozenie.addActionListener(this);
+        dzielenie.addActionListener(this);
+        przecinek.addActionListener(this);
+        rownasie.addActionListener(this);
+        nawias1.addActionListener(this);
+        nawias2.addActionListener(this);
+        del.addActionListener(this);
+        clr.addActionListener(this);
+
+        dodawanie.setFont(czcionka);
+        odejmowanie.setFont(czcionka);
+        mnozenie.setFont(czcionka);
+        dzielenie.setFont(czcionka);
+        przecinek.setFont(czcionka);
+        rownasie.setFont(czcionka);
+        nawias1.setFont(czcionka);
+        nawias2.setFont(czcionka);
+        del.setFont(czcionka);
+        clr.setFont(czcionka);
+
+        //przyciski 0-9:
+        for(int i=0; i<10; i++) {
+            cyfry[i] = new JButton(String.valueOf(i));
+            cyfry[i].addActionListener(this);
+            cyfry[i].setFont(czcionka);
+        }
+
         ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ramka.setSize(650,600);
         ramka.setLayout(null);
         ramka.setVisible(true);
-    }
 
-    public void dodajPrzyciski(){
         wyswietlacz.setBounds(50, 35, 530, 50);
         wyswietlacz.setFont(czcionka);
         wyswietlacz.setEditable(false);
@@ -49,21 +69,21 @@ public class Kalkulator {
 
         panel.setBounds(50, 100, 530, 500);
         panel.setLayout(new GridLayout(6,5,6,6));
-        panel.add(cyfra1);
-        panel.add(cyfra2);
-        panel.add(cyfra3);
+        panel.add(cyfry[1]);
+        panel.add(cyfry[2]);
+        panel.add(cyfry[3]);
         panel.add(mnozenie);
-        panel.add(cyfra4);
-        panel.add(cyfra5);
-        panel.add(cyfra6);
+        panel.add(cyfry[4]);
+        panel.add(cyfry[5]);
+        panel.add(cyfry[6]);
         panel.add(dzielenie);
-        panel.add(cyfra7);
-        panel.add(cyfra8);
-        panel.add(cyfra9);
+        panel.add(cyfry[7]);
+        panel.add(cyfry[8]);
+        panel.add(cyfry[9]);
         panel.add(odejmowanie);
-        panel.add(cyfra0);
-        panel.add(potega);
-        panel.add(pierwiastek);
+        panel.add(cyfry[0]);
+        panel.add(nawias1);
+        panel.add(nawias2);
         panel.add(dodawanie);
         panel.add(del);
         panel.add(clr);
@@ -73,7 +93,14 @@ public class Kalkulator {
         ramka.add(panel);
     }
 
+
     public static void main(String[] args) {
         Kalkulator kalk = new Kalkulator();  // obiekt klasy kalkulator
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+
+        }
 }
